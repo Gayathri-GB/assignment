af939f62b861df71d1891e3ea577f6d04bf8cf21
Gayathri-GB <60889885+Gayathri-GB@users.noreply.github.com>
2021-02-28 18:53:37 +0530

    Add files via upload

---
+++ assignment.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.util.*;
+class Item {
+  String name;
+  int price;
+
+  public Item(String name, int price) {
+    this.name = name;
+    this.price = price;
+  }
+
+  public String toString() { 
+      return this.name + ": " + this.price;
+  }
+}
+
+public class assignment {
+  public static void main(String[] args)throws IOException{
+     String path= "C:/assement/sample_input.txt";
+    FileInputStream fis=new FileInputStream(path);  
+     int ch= fis.read();
+     System.out.println(ch);
+    Scanner sc=new Scanner(fis);
+    int number_of_employees = Integer.parseInt(sc.nextLine().split(": ")[1]);
+    sc.nextLine(); sc.nextLine(); sc.nextLine();
+
+    ArrayList<Item> goodies_items = new ArrayList<Item>();
+
+    while(sc.hasNextLine())  
+    {
+      String current[] = sc.nextLine().split(": ");
+      goodies_items.add(new Item(current[0], Integer.parseInt(current[1])));
+    }
+    sc.close();
+    
+
+    Collections.sort(goodies_items, new Comparator<Item>(){
+      public int compare(Item a, Item b) { 
+        return a.price - b.price; 
+      } 
+    });
+
+    int min_diff = goodies_items.get(goodies_items.size()-1).price;
+    int min_index = 0;
+    for(int i=0;i<goodies_items.size()-number_of_employees+1;i++) {
+    int diff = goodies_items.get(number_of_employees+i-1).price-goodies_items.get(i).price;
+
+      if(diff<=min_diff) {
+        min_diff = diff;
+        min_index = i;
+      }
+    }
+    
+    
+    String opath= "C:/assement/sample_output.txt";
+    FileWriter fw = new FileWriter(opath);
+    fw.write("The goodies selected for distribution are:\n\n");
+    for(int i=min_index;i<min_index + number_of_employees; i++) {
+      fw.write(goodies_items.get(i).toString() + "\n");
+    }
+
+    fw.write("\n And the difference between the chosen goodie with highest price and the lowest price is " + min_diff);
+	  fw.close();
+  }
+}
